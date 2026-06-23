d700f0bc747300c0670707fadc20145d0f7c5d2a
maycon_v_f <maycon.fedechen@escola.pr.gov.br>
2026-06-23 15:20:23 +0000

    .

--- removendo-notas.js
+++ removendo-notas.js
@@ -5,7 +5,7 @@ notas.pop();
 
 const media = ( notas[0] + notas [1] + notas [2] + notas [3]) / notas.length;
 
+ console.log(` A média é ${media}. `);
 
-console.log( media);
-
+console.log(` ideal é {}. `);
                      
