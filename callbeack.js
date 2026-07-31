206cbb2ccbe05128f6808250730e350849e25d83
maycon_v_f <maycon.fedechen@escola.pr.gov.br>
2026-07-31 15:15:42 +0000

    .

---
+++ callbeack.js
@@ -0,0 +1,13 @@
+const nomes = [" Evaldo", "Mari", "Camis"];
+//nomes.forEach(function(nome{
+  //  console.log(nome)
+//});
+//nomes.forEach((nomes) => {
+  //  console.log(nome);
+//});
+function imprimeNome(nome){
+
+console.log(nome);
+}
+
+nomes.forEach(imprimeNome);
