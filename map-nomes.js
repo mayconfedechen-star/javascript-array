7d56952e17d9709d271335bc89812f5221969f87
maycon_v_f <maycon.fedechen@escola.pr.gov.br>
2026-08-06 14:43:20 +0000

    .

---
+++ map-nomes.js
@@ -0,0 +1,7 @@
+const nomes = [" maycon vinicius " , "lucas wendel ","LUAN heenrique "];
+
+const nomesplanorizado = nomes.map (( nome)=>{
+return  nome.toUpperCase();
+});
+
+console.log( nomesplanorisazado);
